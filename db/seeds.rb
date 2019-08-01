0b2ef04358883034cea9cb8a2540e574765296b4
iroshiva <jean.weil@protonmail.com>
2019-08-01 17:00:21 +0200

    end airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,52 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+
+Dog.destroy_all
+
+100.times do
+	dog = Dog.create(
+		name: Faker::Creature::Dog.name,
+		city: City.all.sample
+		)
+end
+# création de 100 dogs
+
+
+Dogsitter.destroy_all
+
+30.times do
+  dogsitter = Dogsitter.create(
+  	first_name: Faker::Name.first_name,
+  	city: City.all.sample 
+  	)
+end
+# création de 30 dogsitters
+
+
+
+City.destroy_all
+
+30.times do
+	city = City.create(
+		name: ["Marseille", "Lille", "Dijon", "Souffelweyersheim", "La Motte Chaloncon", "Vesoul", "Biarritz", "Clermont-Ferrand"].sample
+		)
+end
+# création de 30 villes
+
+
+Stroll.destroy_all
+
+70.times do
+	dog = Dog.all.sample
+	dogsitter = Dogsitter.all.sample
+
+	while dogsitter.city != dog.city
+		dog = Dog.all.sample
+	end
+  Stroll.create(
+  	dog: dog,
+  	dogsitter: dogsitter
+  	)
+end
+# création de 70 promenades
