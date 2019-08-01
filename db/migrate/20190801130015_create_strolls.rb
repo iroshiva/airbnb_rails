8b7a56c14827d6fb94fb326f9bbd56a4154074c0
iroshiva <jean.weil@protonmail.com>
2019-08-01 22:01:59 +0200

    commentaires

--- db/migrate/20190801130015_create_strolls.rb
+++ db/migrate/20190801130015_create_strolls.rb
@@ -2,7 +2,11 @@ class CreateStrolls < ActiveRecord::Migration[5.2]
   def change
     create_table :strolls do |t|
       t.belongs_to :dogsitter, index: true
+      # permet de créer un dogsitter_id dans stroll
+
     	t.belongs_to :dog, index: true
+    	# permet de créer un dog_id dans stroll
+    	
       t.timestamps
     end
   end
--- db/migrate/20190801134302_add_index_to_dogdog.rb
+++ db/migrate/20190801134302_add_index_to_dogdog.rb
@@ -1,6 +1,9 @@
 class AddIndexToDogdog < ActiveRecord::Migration[5.2]
   def change
   	add_reference :dogs, :city, foreign_key: true
+  	# permet de créer un city_id dans dog
+
   	add_reference :dogsitters, :city, foreign_key: true
+  	# permet de créer un city_id dans dogsitter
   end
 end
