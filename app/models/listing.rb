8a74904a4e4602b4544c054426953ea47ff3a96d
iroshiva <jean.weil@protonmail.com>
2019-08-02 17:03:22 +0200

    rails new + migrations + models + links

---
+++ app/models/listing.rb
@@ -0,0 +1,21 @@
+class Listing < ApplicationRecord
+
+	belongs_to :city
+	belongs_to :user
+	has_many :reservations
+
+	validates :available_beds,
+		presence: true,
+		numericality: { greater_than: 0 }
+
+	validates :price,
+		presence: true,
+		numericality: { greater_than: 0 }
+
+	validates :description,
+		presence: true,
+		length: { minimum: 140 }
+
+	validates :welcome_message,
+		presence: true
+end
---
+++ db/migrate/20190802145310_add_index_to_reservation.rb
@@ -0,0 +1,6 @@
+class AddIndexToReservation < ActiveRecord::Migration[5.2]
+  def change
+  	add_reference :reservations, :user, foreign_key: true
+  	add_reference :reservations, :listing, foreign_key: true
+  end
+end
